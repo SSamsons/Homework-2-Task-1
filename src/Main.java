21a217a1f6d6aefd05d8361612579feb885fcf52
Stanislav <people.ss@yandex.ru>
2023-03-12 23:44:58 +0300

    corrected commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int amount = 25683;
+        int bonus = 20;
+
+        int balance = (amount / bonus);
+
+        System.out.println("Начисленные мили на Ваш счет: " + balance);
     }
 }
